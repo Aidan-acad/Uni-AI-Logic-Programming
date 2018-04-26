cc731329b1e774c3ff996316218d7cd4983be6e8
Gregory Sims <briggybros.2@googlemail.com>
2018-04-26 15:38:22 +0100

    Split charging and oracle in preprocess

--- assignment2_wp_23191.pl
+++ assignment2_wp_23191.pl
@@ -106,6 +106,10 @@ content_search(Targets, Results):-
   query_world(agent_current_position, [A, P]),
   flood_check(Targets,[],[P],[P],[],Results).
 
-preprocess(Items):-
-  content_search([o(1),o(2),o(3),o(4),o(5),o(6),o(7),o(8),o(9),o(10),c(1),c(2)], Items),!.
+is_oracle((o(_)|_)).
+is_charging((c(_)|_)).
+preprocess(Oracles, Charging):-
+  content_search([o(1),o(2),o(3),o(4),o(5),o(6),o(7),o(8),o(9),o(10),c(1),c(2)], Items),!,
+  include(is_oracle, Items, Oracles),
+  include(is_charging, Items, Charging).
   
