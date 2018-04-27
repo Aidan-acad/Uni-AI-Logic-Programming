1c40f274ca120dcec6297ec06f30c2ba70d78f81
Gregory Sims <briggybros.2@googlemail.com>
2018-04-27 23:19:17 +0100

    Really fixed

--- assignment2_wp_23191.pl
+++ assignment2_wp_23191.pl
@@ -2,11 +2,12 @@ candidate_number(12345).
 
 % Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
 % find_identity(-A)
-find_identity(A):-
-  (
-    part_module(2)   -> find_identity_2(A),!
-  ; otherwise -> find_identity_o(A),!
-  ).
+find_identity(A) :-
+    (   part_module(2)
+    ->  find_identity_2(A), !
+    ;   otherwise
+    ->  find_identity_o(A), !
+    ).
 
 % loop through actors, and their list of links and compare
 % do this until only one actor remains
@@ -58,6 +59,20 @@ getPathCCost(S, [(X| Pos)|Rest], (Y, Path), FB):-
 add(X, Y, Z, A):-
   A is X + Y + Z.
 
+goToNearOracle_FromCharge(S,O,C,L):-
+  write('near oracle from charge'),nl,
+  getPathCost(S, O, (o(-1), []), CB),
+  write('Got path'),nl,
+  CB = (ID,Path),
+  Path = [End|Tail],
+  write(CB),nl,
+  my_agent(Agent),
+  reverse(Path,[_Init|P]),
+  query_world( agent_do_moves, [Agent,P] ),
+  write('asking '),write(ID),nl,
+  query_world( agent_ask_oracle, [Agent, ID, link, L]),
+  write(L),nl.
+
 % this finds nearest oracle, checks you can go there with ur
 % energy, then gets a link from that oracle
 goToNearOracle(S, O, C, L):-
@@ -80,13 +95,14 @@ goToNearOracle(S, O, C, L):-
               write('asking '),write(ID),nl,
               query_world( agent_ask_oracle, [Agent, ID, link, L]),
               write(L),nl
-  ; otherwise ->  getPathCost(S, C, (o(-1),[]), (c(CS),CSPath)),
+  ; otherwise ->  getPathCost(S, C, (o(-1),[]), (CS,CSPath)),
                   reverse(CSPath,[_Init|P]),
                   query_world( agent_do_moves, [Agent,P]),
                   write('charging at '),write(c(CS)),nl,
                   query_world(agent_topup_energy, [Agent,CS]),
+                  write('successfully charged'),nl,
                   CSPath = [Location|PrevPath],
-                  goToNearOracle(Location, O, C, L)
+                  goToNearOracle_FromCharge(Location, O, C, L)
   ).
 
 find_identity_o(A, [A|[]], [], _, _, _).
@@ -150,7 +166,5 @@ is_charging((c(_)|_)).
 preprocess(Oracles, Charging):-
   set_prolog_stack(global, limit(100 000 000 000)),
   content_search([o(1),o(2),o(3),o(4),o(5),o(6),o(7),o(8),o(9),o(10),c(1),c(2)], Items),!,
-  include(is_oracle, Items, TempO),
-  include(is_charging, Items, TempC),
-  reverse(TempO, Oracles),
-  reverse(TempC, Charging). 
+  include(is_oracle, Items, Oracles),
+  include(is_charging, Items, Charging).
